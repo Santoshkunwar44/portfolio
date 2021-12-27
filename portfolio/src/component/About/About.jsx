108593d9bca3686084f1c5fc5e3499fb73a39448
santosh <intelligencechannel86@gmail.com>
2021-12-27 20:26:38 +0545

    initial

--- portfolio/src/component/About/About.jsx
+++ portfolio/src/component/About/About.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 import "./About.css"
 function About() {
     return (
-        <div>
-          About  
+        <div className='a'>
+
+        <div className="a-left">
+          <div className="a-card bg"></div>
+          <div className="a-card">
+            <img src="https://images.pexels.com/photos/7605222/pexels-photo-7605222.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" className='a-img' />
+          </div>
+
+        </div>
+        <div className="a-right">
+          <h1>About Me</h1>
+          <p className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita aut explicabo culpa hic iure exercitationem animi, accusantium, repellat tempore maiores architecto temporibus eveniet.</p>
+          <div className="a-award">
+            <img src="https://images.pexels.com/photos/6565255/pexels-photo-6565255.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="" className='a-right-img' />
+            <p className="a-award-desc">Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nesciunt mollitia et? dolor sit amet consectetur adipisicing elit. Sunt unde debitis architecto!</p>
+          </div>
+      
+        </div>
         </div>
     )
 }
--- portfolio/src/component/Intro/Intro.jsx
+++ portfolio/src/component/Intro/Intro.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import "./Intro.css"
+import React from "react";
+import me from "../../img/me.png";
+import "./Intro.css";
 function Intro() {
-    return (
-        <div>
-            INTOR
+  return (
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello,My Name is </h2>
+          <h1 className="i-name">SANTOSH </h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">WEB DEVELOPER</div>
+              <div className="i-title-item">UI/UX DESIGNER</div>
+              <div className="i-title-item">WEBDEVELOPER</div>
+              <div className="i-title-item">FREELANCER</div>
+              <div className="i-title-item">FREELANCER</div>
+            </div>
+          </div>
+          <div className="i-desc">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequatur distinctio nihil quae modi ipsam rerum aspernatur vero!
+            Lorem ipsum dolor sit.\
+          </div>
         </div>
-    )
+      </div>
+      <div className="i-right">
+        <div className="i-right-bg"> </div>
+        <img src={me} alt="" className="i-img" />
+      </div>
+    </div>
+  );
 }
 
-export default Intro
+export default Intro;
